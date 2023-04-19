a73a473f417bd5f8bebce8d6a28c8cfa7266d401
hoaithuannguyen <nguyenhoaithuanqn@gmail.com>
2023-04-19 22:29:59 +0700

    bài tập tính lãi suất đơn

--- note.js
+++ note.js
@@ -1,11 +1,22 @@
 
 
-let day = +prompt("nhapnamvaoday");
+/*let day = +prompt("nhapnamvaoday");
 let month = +prompt("tháng");
 let year = +prompt("ngày");
 
 console.log ("date", day + "/" + month + "/" + year);
-document.write ("date", day + "/" + month + "/" + year);    
+document.write ("date", day + "/" + month + "/" + year);*/
+
+
+
+let money = +prompt("số tiền bạn vay");
+let n = +prompt("số năm bạn vay");
+let rate = +prompt("phần trăm lãi suất")
+
+pay = money + n * rate;
+
+console.log ("số tiền phải thanh toán:", pay);
+document.write ("số tiền phải thanh toán:", pay);
 
 
 
