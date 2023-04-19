de8ff110ad093f0a31d15fbf6bc2a62a9d1794d5
hoaithuannguyen <nguyenhoaithuanqn@gmail.com>
2023-04-19 21:56:06 +0700

    testjavaScript

---
+++ note.js
@@ -0,0 +1,12 @@
+
+
+let day = +prompt("nhapnamvaoday");
+let month = +prompt("tháng");
+let year = +prompt("ngày");
+
+console.log ("date", day + "/" + month + "/" + year);
+document.write ("date", day + "/" + month + "/" + year);    
+
+
+
+
