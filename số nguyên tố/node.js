789363ba3c49827a0763200ee1e40fb7357f1cd9
Hieu Hoang Ngo <71125098+fantasticroll@users.noreply.github.com>
2023-05-15 23:01:58 +0900

    Update node.js

--- số nguyên tố/node.js
+++ số nguyên tố/node.js
@@ -1,6 +1,6 @@
 let numbers = parseInt(prompt("Số lượng số nguyên tố:"));
 if (numbers < 2) {
-  alert("Số lượng số nguyên tố phải lớn hơn hoặc bằng 2.");
+  alert("Số lượng số nguyên tố phải lớn hơn hoặc bằng 2!");
 } else {
   let count = 0;
   let N = 2;
