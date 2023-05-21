451facbbe3cf7d76e2a071cbd635ccb58955dc5f
nguyenhoangthe93 <nguyenhoangthe93@gmail.com>
2023-05-21 17:59:04 +0700

    TimMaxAndMin

--- lesson/js/Array/TimMax/node.js
+++ lesson/js/Array/TimMax/node.js
@@ -59,3 +59,17 @@ for (let i = 0; i < numbers.length; i++) {
 }
 console.log(`max: " + ${findMax} + " at position " + ${index}`);
 
+
+let numbersMin = [-3, 5, 1, 3, 2, 10];
+let findMin = numbers[0];
+let indexMin = 0;
+
+for (let x = 0; x < numbers.length; x++) {
+  if(numbers[x] < findMin)
+  {
+    findMin = numbers[x];
+    indexMin = x;
+  }
+}
+console.log(`min: " + ${findMin} + " at position " + ${indexMin}`);
+
