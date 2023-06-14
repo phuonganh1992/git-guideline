15969c7e1617e663f4e1044fa855b55cff4c712f
hoaithuannguyen <nguyenhoaithuanqn@gmail.com>
2023-06-14 19:51:21 +0700

    casetudy

---
+++ .vscode/quanlysinhvien.js
@@ -0,0 +1,14 @@
+var sinhvien = [
+    {maso: "SV001",hoten: "Nguyễn Văn A",namsinh: 1998,gioitinh: "Nam",sodienthoai: "0987654321",diachiemail: "nguyenvana@example.com",khoa: "Khoa Kỹ thuật",nganhhoc: "Công nghệ thông tin"},
+    {maso: "SV002",hoten: "Nguyễn Văn B",namsinh: 1998,gioitinh: "Nam",sodienthoai: "0987654321",diachiemail: "nguyenvana@example.com",khoa: "Khoa Kỹ thuật",nganhhoc: "Công nghệ thông tin"},
+    {maso: "SV003",hoten: "Nguyễn Văn C",namsinh: 1998,gioitinh: "Nam",sodienthoai: "0987654321",diachiemail: "nguyenvana@example.com",khoa: "Khoa Kỹ thuật",nganhhoc: "Công nghệ thông tin"},
+    {maso: "SV004",hoten: "Nguyễn Văn D",namsinh: 1998,gioitinh: "Nam",sodienthoai: "0987654321",diachiemail: "nguyenvana@example.com",khoa: "Khoa Kỹ thuật",nganhhoc: "Công nghệ thông tin"},
+    {maso: "SV005",hoten: "Nguyễn Văn E",namsinh: 1998,gioitinh: "Nam",sodienthoai: "0987654321",diachiemail: "nguyenvana@example.com",khoa: "Khoa Kỹ thuật",nganhhoc: "Công nghệ thông tin"},
+    {maso: "SV006",hoten: "Nguyễn Văn F",namsinh: 1998,gioitinh: "Nam",sodienthoai: "0987654321",diachiemail: "nguyenvana@example.com",khoa: "Khoa Kỹ thuật",nganhhoc: "Công nghệ thông tin"},
+    {maso: "SV007",hoten: "Nguyễn Văn G",namsinh: 1998,gioitinh: "Nam",sodienthoai: "0987654321",diachiemail: "nguyenvana@example.com",khoa: "Khoa Kỹ thuật",nganhhoc: "Công nghệ thông tin"},
+    {maso: "SV008",hoten: "Nguyễn Văn H",namsinh: 1998,gioitinh: "Nam",sodienthoai: "0987654321",diachiemail: "nguyenvana@example.com",khoa: "Khoa Kỹ thuật",nganhhoc: "Công nghệ thông tin"},
+    
+  ];
+
+  var table = document.getElementById("student-table");
+  var addForm = document.getElementById("add-form");
