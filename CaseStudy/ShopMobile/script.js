954b62ce072cd028fb6d8f89b1199f4c4b5c90b8
nguyenhoangthe93 <nguyenhoangthe93@gmail.com>
2023-06-14 21:16:07 +0700

    TheMobile

---
+++ CaseStudy/ShopMobile/script.js
@@ -0,0 +1,10 @@
+let tbody = document.querySelector('tbody');
+let addBtn = document.querySelector('.add');
+let form = document.querySelector('.form-wrapper');
+let saveBtn = document.querySelector('.save');
+let cancelBtn =  document.querySelector('.cancel');
+let mobileEle =  document.querySelector('#mobile');
+let priceEle =  document.querySelector('#price');
+let ramEle =  document.querySelector('#ram');
+let storageEle =  document.querySelector('#storage');
+
