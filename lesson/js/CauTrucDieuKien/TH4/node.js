3e0b2126d39b86350f06e6296a271f5ba230d32a
nguyenhoangthe93 <nguyenhoangthe93@gmail.com>
2023-06-17 13:56:58 +0700

    commit

---
+++ lesson/js/CauTrucDieuKien/TH4/node.js
@@ -0,0 +1,13 @@
+let a = prompt("a: ");
+let b = prompt("b: ");
+
+result = (a + b < 4) ? 'Below' : 'Over';
+
+let message = prompt("Nhap Employee  : ");
+let message = (login == 'Employee') ?
+    'Hello' :
+    (login == 'Director') ?
+      'Greetings' :
+      (login == '') ?
+        'No login' :
+        '';
