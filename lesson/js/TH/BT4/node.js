78fa274691eabcc7450209c61332a327e0a371e0
nguyenhoangthe93 <nguyenhoangthe93@gmail.com>
2023-06-17 12:00:29 +0700

    commit

---
+++ lesson/js/TH/BT4/node.js
@@ -0,0 +1,7 @@
+let name = prompt("Nhập tên: ");
+let address = prompt("Nhập địa chỉ: ");
+let date = prompt("Nhập ngày: ");
+
+document.getElementById("name").innerHTML = name;
+document.getElementById("address").innerHTML = address;
+document.getElementById("date").innerHTML = date;
